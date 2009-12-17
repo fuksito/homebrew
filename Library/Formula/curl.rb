e5aa0ff7cf2d17f14914105b1ae18cba7245116b
Adam Vandenberg <flangy@gmail.com>
2009-12-17 11:55:27 -0800

    curl 7.21 (OS X 10.5 comes with 7.16.3)
    This was necessary because the version which ships with Mac OS X 10.6 is
    old enough to have a bug which causes PyCurl to always report the
    response headers as empty.

---
+++ Library/Formula/curl.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Curl <Formula
+  url 'http://curl.haxx.se/download/curl-7.21.0.tar.bz2'
+  homepage 'http://curl.haxx.se/'
+  md5 'e1a2a773e93a39f3c04cab92c55bf197'
+
+  depends_on 'libssh2' => :optional
+
+  def install
+    # if libssh2, then include it in ./configure
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--with-libssh2"
+    system "make install"
+  end
+end
