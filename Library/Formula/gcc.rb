9b8d152d674f31b384f53526cabc2b848d11d1bc
Jon Mulder <mulderje@muohio.edu>
2010-05-03 19:57:42 -0400

    Add support for gcc-4.5.0 release.
    So far I have only tested it on my laptop (Core 2 (x86) MacBook Pro)
    compiling for c and c++ only.  It should work for everything but Ada
    as I have not looked at the dependencies for that as of yet.  I have
    never done anything with Ruby before, so I am sure there are some things
    that are going to need to be updated, especially the way I take in options.
    On that note I have an option that will build gcc and friends with the -4.5
    suffix so it will not mess with other things, but it is not enabled by default.
    I checked with otool, and it is taking it, but it is doing so from the Cellar,
    and not from /usr/local (which is my prefix).  I did it this way because I
    figured that gcc was looking for, and compiled with specific versions of gmp,
    mpfr, and mpc, and that it should be linked to that location.  Currently I have
    it set up to only compile c and c++, as that is all that I have tested, and did
    not want to send out an untested product (gcc defaults to installing everything
    but Ada and Obj-C++).  I thought it appropreate to add aliases for both g++ and
    gfortran, I also made it so that when the install was envoked from that alias
    that it would default to adding fortran to the default list of installed languages.
    At the same time, I do not remove c or c++ from the default, and I do this only
    because I have not fully looked into what depends on what yet.  Also, I just
    compiled llvm/clang with this, and it is running just fine, just as one random
    test case
    
    TODO:
    Figure out a better way to deal with options
    Add support for gcc trunk
    Add minimum version number to dependencies (couldn't find how to do that)
    Add the ability to run gccs' test suite
    Generally test it more to make sure everything is all still working

---
+++ Library/Formula/gcc.rb
@@ -0,0 +1,94 @@
+require 'formula'
+
+class Gcc <Formula
+  url 'ftp://ftp.gnu.org/gnu/gcc/gcc-4.5.0/gcc-4.5.0.tar.bz2'
+  homepage 'http://gcc.gnu.org'
+  md5 'ff27b7c4a5d5060c8a8543a44abca31f'
+
+  depends_on 'automake' # 1.11.1
+  depends_on 'autoconf' # 2.64
+  depends_on 'gmp'      # >= 4.3.2
+  depends_on 'libmpc'   # >= 0.8.1
+  depends_on 'mpfr'     # >= 2.4.2
+
+  aka 'g++'
+  aka 'gfortran'
+  
+  # FIXME: Include support for Ada at some point, there are other dependencies,
+  # and I have never used it, so have not explored it yet (GNAT Compiler).
+  avail_langs = ['c', 'c++', 'fortran', 'java', 'objc', 'objc++']
+  
+  # FIXME: I don't know ruby, and I am sure this is a much more elegant solution for this.
+  def options 
+    [
+      ["--with-suffix", "Add #{version.slice(/\d\.\d/)} to the suffix of the commands"],
+      ["--enable-fortran", "Enable Fortran language support"],
+      ["--enable-java", "Enable Java languagesupport"],
+      ["--enable-objc", "Enable Objective-C language support"],
+      ["--enable-objc++", "Enable Objective-C++ language support"],
+      ["--disable-c", "Disable C language support"],
+      ["--disable-c++", "Disable C++ language support"]
+      #avail_langs.map { |e| ["--enable-#{e}", "enable #{e} language support"] },
+      #avail_langs.map { |e| ["--disable-#{e}", "disable #{e} language support"] }
+    ]
+  end
+  
+  def install
+    # Remove the built in flags to get it to build properly
+    # TODO: LDFLAGS and CPPFLAGS probably don't need to be in here
+    %w[MAKEFLAGS CFLAGS LDFLAGS CPPFLAGS CXXFLAGS CXX LD CC].each do |flag|
+      ENV.delete(flag)  
+    end
+    
+    # Get the prefixs' for the already installed dependencies.
+    gmp = Formula.factory 'gmp'
+    limpc = Formula.factory 'libmpc'
+    mpfr = Formula.factory 'mpfr'
+    
+    # This is not the default for GCC (which is all but Ada and Obj-C++)
+    base_langs = ['c', 'c++']
+    
+    # Automatically tack on fortran if user trys to install from 'gfortran' alias
+    base_langs << 'fortran' if ARGV.include? 'gfortran'
+    
+    # FIXME: Create a better, more ruby, way format for this.
+    ARGV.each do |arg|
+      case curr = arg
+      when '--enable-fortran'
+        base_langs << 'fortran'
+      when '--enable-java'
+        base_langs << 'java'
+      when '--enable-objc'
+        base_langs << 'objc'
+      when '--enable-objc++'
+        base_langs << 'objc++'
+      when '--disable-c'
+        base_langs.delete 'c'
+      when '--disable-c++'
+        base_langs.delete 'c++'
+      else 'Sorry, we are working on that...'
+      end
+    end
+    
+    args = 
+      [
+        "--prefix=#{prefix}", "--libexecdir=#{libexec}", "--libdir=#{lib}",
+        "--mandir=#{man}", "--infodir=#{info}", # "--includedir=#{include}",
+        "--with-gmp=#{gmp.prefix}", "--with-mpfr=#{mpfr.prefix}", "--with-mpc=#{libmpc.prefix}",
+        "--enable-languages=#{base_langs.uniq.join(',')}"
+      ]
+      
+    args << "--program-suffix=-#{version.slice(/\d\.\d/)}" if ARGV.include? "--with-suffix"
+
+    # FIXME: This may be able to be taken out at this point, this was from early on when
+    # the include directories and *FLAGS were not working properly.
+    # When building there were errors stating that it was "Waiting for unfinished jobs"
+    ENV.j1
+    system "./configure", *args
+    system "make"
+    
+    # TODO: Implement the make checks at some point.
+    
+    system "make install"
+  end
+end
