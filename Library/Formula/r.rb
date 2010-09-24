7da2909d02c5fd448b48ed1cbb42392fd5b0bb85
Tianyi Cui <tianyicui@gmail.com>
2010-09-25 07:55:43 +0800

    r: make it depend on gfortran explicitly.

--- Library/Formula/r.rb
+++ Library/Formula/r.rb
@@ -5,13 +5,9 @@ class R <Formula
   homepage 'http://www.R-project.org/'
   md5 '7421108ade3e9223263394b9bbe277ce'
 
-  def install
-    unless `/usr/bin/which gfortran`.chomp.size > 0
-      opoo 'No gfortran found in path'
-      puts "You'll need to `brew install gfortran` or otherwise have a copy"
-      puts "of gfortran in your path for this brew to work."
-    end
+  depends_on 'gfortran'
 
+  def install
     ENV["FCFLAGS"] = ENV["CFLAGS"]
     ENV["FFLAGS"]  = ENV["CFLAGS"]
 
@@ -30,13 +26,5 @@ class R <Formula
     ln_s prefix+"R.framework/Resources/bin/Rscript", bin
     ln_s prefix+"R.framework/Resources/man1/R.1", man1
     ln_s prefix+"R.framework/Resources/man1/Rscript.1", man1
-  end
-
-  def caveats; <<-EOS.undent
-    R requires a fortran compiler to install.
-    You can install gfortran using Homebrew:
-        brew install gfortran
-
-    EOS
   end
 end
