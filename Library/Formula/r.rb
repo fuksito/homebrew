9a35f4fd5251a74d34910f9dc12f21afd886dcf4
Tianyi Cui <tianyicui@gmail.com>
2010-09-25 07:55:43 +0800

    r: make it depend on gfortran when no gfortran executable is found

--- Library/Formula/r.rb
+++ Library/Formula/r.rb
@@ -1,19 +1,22 @@
 require 'formula'
 
+def which_gfortran
+  `/usr/bin/which gfortran`.strip
+end
+
 class R <Formula
   url 'http://cran.r-project.org/src/base/R-2/R-2.11.1.tar.gz'
   homepage 'http://www.R-project.org/'
   md5 '7421108ade3e9223263394b9bbe277ce'
 
-  def install
-    unless `/usr/bin/which gfortran`.chomp.size > 0
-      opoo 'No gfortran found in path'
-      puts "You'll need to `brew install gfortran` or otherwise have a copy"
-      puts "of gfortran in your path for this brew to work."
-    end
+  depends_on 'gfortran' if which_gfortran.empty?
 
-    ENV["FCFLAGS"] = ENV["CFLAGS"]
-    ENV["FFLAGS"]  = ENV["CFLAGS"]
+  def install
+    # Select the Fortran compiler to be used:
+    ENV["FC"] = ENV["F77"] = which_gfortran
+
+    # Set Fortran optimization flags:
+    ENV["FFLAGS"] = ENV["FCFLAGS"] = ENV["CFLAGS"]
 
     system "./configure", "--prefix=#{prefix}", "--with-aqua", "--enable-R-framework",
            "--with-lapack"
@@ -30,13 +33,5 @@ class R <Formula
     ln_s prefix+"R.framework/Resources/bin/Rscript", bin
     ln_s prefix+"R.framework/Resources/man1/R.1", man1
     ln_s prefix+"R.framework/Resources/man1/Rscript.1", man1
-  end
-
-  def caveats; <<-EOS.undent
-    R requires a fortran compiler to install.
-    You can install gfortran using Homebrew:
-        brew install gfortran
-
-    EOS
   end
 end
