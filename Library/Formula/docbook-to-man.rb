a333cd390ca3aa7caaaeab6cc1aa9976a983f396
Tianyi Cui <tianyicui@gmail.com>
2010-10-05 21:16:05 +0800

    New formula: docbook-to-man

---
+++ Library/Formula/docbook-to-man.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class DocbookToMan <Formula
+  url 'http://www.oasis-open.org/docbook/tools/dtm/docbook-to-man.tar.gz'
+  version '2.0.0'
+  homepage 'http://www.oasis-open.org/docbook/tools/dtm/'
+  md5 'b28ddaaa8eb4b775100c67fd1205240a'
+
+  def install
+    mkdir_p lib
+    system "make install ROOT=#{prefix}"
+  end
+end
