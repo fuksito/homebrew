223dcda26862cc6c8f5d20c6d44a5421f6c1524c
Tianyi Cui <tianyicui@gmail.com>
2010-10-08 19:12:16 +0800

    wip

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -6,13 +6,14 @@ class Clang <Formula
   url       'http://llvm.org/releases/2.8/clang-2.8.tgz'
   homepage  'http://llvm.org/'
   md5       '10e14c901fc3728eecbd5b829e011b59'
+  head      'git://repo.or.cz/clang.git'
 end
 
 class Llvm <Formula
   url       'http://llvm.org/releases/2.8/llvm-2.8.tgz'
   homepage  'http://llvm.org/'
   md5       '220d361b4d17051ff4bb21c64abe05ba'
-  # TODO: add head here
+  head      'git://repo.or.cz/llvm.git'
 
   def options
     [['--with-clang', 'Also build & install clang']]
@@ -27,7 +28,6 @@ class Llvm <Formula
     end
 
     system "./configure", "--prefix=#{prefix}",
-                          "--enable-targets=host-only",
                           "--enable-optimized",
                           "--enable-bindings=none",
                           "--disable-assertions",
