ce8c01f9746896a5c005293aff5fb0c789201f82
Tianyi Cui <tianyicui@gmail.com>
2010-11-03 14:38:01 +0800

    openssh: 5.6p1 & hpn

--- Library/Formula/openssh.rb
+++ Library/Formula/openssh.rb
@@ -1,10 +1,14 @@
 require 'formula'
 
 class Openssh <Formula
-  url 'ftp://ftp.lambdaserver.com/pub/OpenBSD/OpenSSH/portable/openssh-5.4p1.tar.gz'
+  url 'ftp://ftp.lambdaserver.com/pub/OpenBSD/OpenSSH/portable/openssh-5.6p1.tar.gz'
   homepage 'http://www.openssh.com/'
-  md5 'da10af8a789fa2e83e3635f3a1b76f5e'
-  version '5.4p1'
+  md5 'e6ee52e47c768bf0ec42a232b5d18fb0'
+  version '5.6p1'
+
+  def patches
+    "http://www.psc.edu/networking/projects/hpn-ssh/openssh-5.6p1-hpn13v10.diff.gz" if ARGV.include? '--enable-hpn'
+  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--with-libedit", "--prefix=#{prefix}"
