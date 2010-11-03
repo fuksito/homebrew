9204586e2da0a29b3c0d673d7939115173dd9833
Tianyi Cui <tianyicui@gmail.com>
2010-11-03 15:16:00 +0800

    LLVM: Use official SVN for head

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -14,14 +14,14 @@ class Clang <Formula
   url       'http://llvm.org/releases/2.8/clang-2.8.tgz'
   homepage  'http://llvm.org/'
   md5       '10e14c901fc3728eecbd5b829e011b59'
-  head      'git://repo.or.cz/clang.git'
+  head      'http://llvm.org/svn/llvm-project/cfe/trunk', :using => :svn
 end
 
 class Llvm <Formula
   url       'http://llvm.org/releases/2.8/llvm-2.8.tgz'
   homepage  'http://llvm.org/'
   md5       '220d361b4d17051ff4bb21c64abe05ba'
-  head      'git://repo.or.cz/llvm.git'
+  head      'http://llvm.org/svn/llvm-project/llvm/trunk', :using => :svn
 
   def options
     [
