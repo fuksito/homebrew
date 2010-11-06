fdb0eae99560e1b8c4df22b94f4395f7a33d141d
Tianyi Cui <tianyicui@gmail.com>
2010-11-06 09:08:42 +0800

    wip

--- Library/Formula/sbcl.rb
+++ Library/Formula/sbcl.rb
@@ -1,22 +1,41 @@
 require 'formula'
-require 'hardware'
 
-class Sbcl <Formula
-  if snow_leopard_64?
-    url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.29/sbcl-1.0.29-x86_64-darwin-binary-r2.tar.bz2'
-    md5 '47c99c60ec44e57070807c0890ba1c90'
-  else
-    url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.29/sbcl-1.0.29-x86-darwin-binary-r2.tar.bz2'
-    md5 '6e6b027a5fd05ef0c8faee30d89ffe54'
-  end
+# The official binary for 1.0.29 is used to bootstrap the latest version.
+class SbclBinary < Formula
+  url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.29/sbcl-1.0.29-x86-darwin-binary-r2.tar.bz2'
+  md5 '6e6b027a5fd05ef0c8faee30d89ffe54'
   version '1.0.29'
-  homepage 'http://www.sbcl.org/'
+end
 
-  skip_clean 'bin'
-  skip_clean 'lib'
+class Sbcl < Formula
+  url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.43/sbcl-1.0.43-source.tar.bz2'
+  homepage 'http://www.sbcl.org/'
+  md5 '2b125844371881a99cfdf63c286e74cd'
 
   def install
-    ENV['INSTALL_ROOT'] = prefix
-    system "sh install.sh"
+    original_path = pwd
+    puts "original_path=#{original_path}"
+    mktemp do
+      # install 1.0.29 to a temporary location
+      binary_path = pwd
+      SbclBinary.new('sbcl-binary').brew do
+        ENV['INSTALL_ROOT'] = binary_path
+        system "sh install.sh"
+        puts "binary_path=#{binary_path}"
+      end
+      # use the temporary binary SBCL in build
+      ENV.prepend 'PATH', "#{binary_path}/bin", ':'
+      ENV['SBCL_HOME'] = "#{binary_path}/lib/sbcl"
+      # build and install 1.0.43
+      cd original_path do
+        system "sh make.sh --prefix=#{prefix}"
+        cd 'tests' do
+          system "sh run-tests.sh"
+        end
+        ENV['INSTALL_ROOT'] = prefix
+        system "sh install.sh"
+      end
+    end
   end
+
 end
