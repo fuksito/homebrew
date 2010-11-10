41ab9eecccd65ce0a6c8bfcb2bf9dd71b4b1baef
Tianyi Cui <tianyicui@gmail.com>
2010-11-11 02:16:28 +0800

    Use https for all GitHub URLs

--- Library/Formula/liblastfm.rb
+++ Library/Formula/liblastfm.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Liblastfm <Formula
   homepage 'http://github.com/mxcl/liblastfm/'
-  url 'http://github.com/mxcl/liblastfm/tarball/0.3.3'
+  url 'https://github.com/mxcl/liblastfm/tarball/0.3.3'
   md5 'fe339bf46aefc515c251200d10262f79'
 
   depends_on 'qt'
