cde52e3f3f813e9583cf46a6d489007fee3fc45c
Tianyi Cui <tianyicui@gmail.com>
2010-12-17 19:32:19 +0800

    asciidoc updated

--- Library/Formula/asciidoc.rb
+++ Library/Formula/asciidoc.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Asciidoc <Formula
-  url 'http://downloads.sourceforge.net/project/asciidoc/asciidoc/8.6.1/asciidoc-8.6.1.tar.gz'
-  md5 '3d16b16a953c0c2d00b6a6d089882d0e'
+  url 'http://downloads.sourceforge.net/project/asciidoc/asciidoc/8.6.3/asciidoc-8.6.3.tar.gz'
+  md5 '1ef39786ee1b4c8a788584e73db2c55a'
   homepage 'http://www.methods.co.nz/asciidoc'
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
+    system "make"
     system "make install"
   end
 end
