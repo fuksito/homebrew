420eff6d76a10ade37284d13ad0299a5ee0f0f75
Tianyi Cui <tianyicui@gmail.com>
2010-12-21 09:53:26 +0800

    binutils

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -1,9 +1,8 @@
 require 'formula'
 
 class Binutils <Formula
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.20.tar.gz'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.21.tar.gz'
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
-  md5 'e99487e0c4343d6fa68b7c464ff4a962'
 
   def options
     [['--default-names', "Do NOT prepend 'g' to the binary; will override system utils."]]
