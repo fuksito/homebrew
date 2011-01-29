90dc218e8b6558e62abf9a6a78bbd3d83427df0b
Adam Vandenberg <flangy@gmail.com>
2011-01-29 13:55:44 -0800

    vim 7.3.107

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Vim <Formula
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '951641b8784de5175cadf9fdbc361db2b6b45060'
-  version '7.3.102'
+  url 'https://vim.googlecode.com/hg/', :revision => '26fb122355d4d6b04850f7608f1d82da8dbc51c7'
+  version '7.3.107'
   homepage 'http://www.vim.org/'
 
   head 'https://vim.googlecode.com/hg/'
